7b2db6a80ef3c4892128bf8d6fe9d8d845c01974
Scr4pplee <sentinel.3313@gmail.com>
2026-07-29 21:58:38 +0700

    Remove obsolete index.js bridge (renamed to server.js)

--- index.js
+++
@@ -1,4 +0,0 @@
-// File jembatan untuk Hostinger: "Direktori root" & "Startup file" default Node.js App
-// di Hostinger tidak bisa diarahkan langsung ke folder server/, jadi file ini di root
-// yang jadi startup file-nya, dan cuma memanggil kode backend beneran di server/index.js.
-require('./server/index.js');
